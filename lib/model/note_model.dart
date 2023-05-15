ae621c39989780554868c561885be6bff2ba00d3
Ahmed Ahmed Ibrahim <ahmed.ibrahim6894@gmail.com>
2023-05-15 03:31:31 +0300

    - init hive database  - make a model to store  - generate typeAdapter to store an object

---
+++ lib/model/note_model.dart
@@ -0,0 +1,52 @@
+import 'package:hive/hive.dart';
+
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+
+  @HiveField(0)
+  final String title;
+
+  @HiveField(1)
+  final String subTitle;
+
+  @HiveField(2)
+  final String date;
+
+  @HiveField(3)
+  final int color;
+
+  NoteModel(
+      {required this.title,
+      required this.subTitle,
+      required this.date,
+      required this.color});
+}
+
+
+/*
+
+  1- creating model
+
+  2- adding package hive and hive_flutter
+
+  3- adding hive_generator and build_runner in dev_dependencies
+
+  4- coding
+
+  5- make the model extend hiveObject
+
+  6- give the model typeId    --- this is 0 object   the first one
+
+  7- give hiveField to every variable u have
+
+  8- make the part (like the import ) to make him know which file he will generate
+
+  9- go to terminal and write flutter packages pub run build_runner build
+      to generate the type adapter
+
+  10-
+
+ */
